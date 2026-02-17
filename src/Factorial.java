0a80654cb1c2f5661d76fcef8c40af5b07fe8b10
Arpith Joseph <arpithjoseph2005@gmail.com>
2026-02-17 21:43:30 +0530

    Recheck

--- src/Factorial.java
+++ src/Factorial.java
@@ -18,6 +18,7 @@ public class Factorial {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        System.out.println(factorial(n));
+        long fact = factorial(n);
+        System.out.println(fact);
     }
 }
